d8481d25c6e39c452b89e9fd161a8e8363246d03
MadisonVT <122593710+MadisonVT@users.noreply.github.com>
2024-05-17 23:44:22 -0600

    Updated my challenge test code

--- Unit 2/Challenge/Code_Test_Challenge_2.cs
+++ Unit 2/Challenge/Code_Test_Challenge_2.cs
@@ -15,10 +15,22 @@ namespace MyApplication
       //Writes their name to the console with a welcome message.
       int Cool_Num = 14;
       //Testing a number variable
+      float Float_Num = 2.0f;
+      //Testing a float variable
+      bool Bool_Var = true;
+      //Testing a boolean variable
+      string String_Var = "Cool Numbers and Variables!";
+      //Testing a string variable
       int Cooler_Num = Cool_Num / 2;
       //Dividing the number variable by 2
+      Console.WriteLine(String_Var);
+      //Writes a message to the console
       Console.WriteLine(Cooler_Num);
       //Writes quotient to the console.
+      Console.WriteLine(Float_Num);
+      //Writes a float to the console.
+      Console.WriteLine(Bool_Var);
+      //Writes the boolean value to the console.
      }
   }
 }
