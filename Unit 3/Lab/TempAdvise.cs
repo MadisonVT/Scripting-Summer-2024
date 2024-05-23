04aad9ded7ee74461b423c724850bb757d3f3d56
MadisonVT <122593710+MadisonVT@users.noreply.github.com>
2024-05-22 21:42:35 -0600

    Added comments to Temp Advisor

--- Unit 3/Lab/TempAdvise.cs
+++ Unit 3/Lab/TempAdvise.cs
@@ -6,10 +6,15 @@ namespace Program
         public static void Main()
         {
             Console.WriteLine("What is the temperature in Celsius right now?");
+            //Prompts user for a temperature in Celsius.
             int temp;
+            //Creates an empty int variable.
             temp = Convert.ToInt32(Console.ReadLine());
+            //Assigns an int user input to the int "temp" variable.
             string result = (temp > 30) ? "Stay in the shade and drink plenty of water!" : "Enjoy the nice weather!";
+            //If statement with a different message depending on wether the input is above or below 30.
             Console.WriteLine(result);
+            //Prints the result of the If statement to the console.
         }
     }
 }
