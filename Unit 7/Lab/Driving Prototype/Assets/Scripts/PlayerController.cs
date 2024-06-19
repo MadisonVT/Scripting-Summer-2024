99fb233e2abf6cff08062f181b79ac923b26a422
MadisonVT <10995165@uvu.edu>
2024-06-18 23:35:22 -0600

    Driving Sim Finished

--- Unit 7/Lab/Driving Prototype/Assets/Scripts/PlayerController.cs
+++ Unit 7/Lab/Driving Prototype/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,23 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+
         
     }
 
     // Update is called once per frame
+    private float horizontalInput = 25;
+    private float forwardInput = 30;
+    private float turnSpeed = 40;
+    private float speed = 27;
     void Update()
     {
-        
+        // Move the vehicle forward
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates car based on horizontal input
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
